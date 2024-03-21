e960342a9b9356b24d0ddf1d188b628721eab168
TranDangKhoa <khoa.tran.tester@gmail.com>
2024-03-21 11:08:56 +0700

    New TC

--- src/WebDriver/SetUpDriver.java
+++ src/WebDriver/SetUpDriver.java
@@ -1,21 +1,22 @@
 package WebDriver;
 
-import java.util.*;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.*;
 
 public class SetUpDriver {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 	
-	@BeforeClass
-	public void BeforeClass() {
-	//	System.setProperty("webdriver.chrome.driver",projectPath + "\\webDriver\\chromedriver.exe");)
-		driver =  new ChromeDriver();
-	}
+	 
 	@Test
 	public void TC_01() {
-		driver.get("https://dev.ecomnet.app/");
+		 System.setProperty("webdriver.edge.driver", projectPath +"\\webDriver\\msedgedriver.exe");
+		 driver = new EdgeDriver();
+		 driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		 driver.get("https://dev.ecomnet.app/");
+		 driver.quit();
 	}
 }
