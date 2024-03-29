651c0d8ee36b77273b64f21222d92c60021bcc09
TranDangKhoa <khoa.tran.tester@gmail.com>
2024-03-29 23:15:59 +0700

    TC_02 Handle custom dropdown list

--- src/WebDriver/SetUpDriver.java
+++ src/WebDriver/SetUpDriver.java
@@ -1,7 +1,8 @@
 package WebDriver;
 
+//import java.util.ArrayList;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -15,7 +16,7 @@ import org.testng.annotations.*;
 public class SetUpDriver {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
-	Actions act;
+	//Actions act;
 	
 	@BeforeClass
 	public void beforeClass() {
@@ -24,7 +25,7 @@ public class SetUpDriver {
 		 driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		 driver.get("https://dev.ecomnet.app/");
 		 driver.manage().window().maximize();
-		 act = new Actions(driver);
+		 //act = new Actions(driver);
 		 
 	}
 	@Test
@@ -45,15 +46,31 @@ public class SetUpDriver {
 		 //searchbox.sendKeys(Keys.chord(Keys.CONTROL,"a"));
 		 searchbox.sendKeys(Keys.ENTER);
 		 Thread.sleep(3000);
-}
-	
-//	@Test
-//	public void TC_02_Search() {
-//		driver.findElement(By.xpath("//input[@name = 'email']")).sendKeys("bkaadmin");;
-//		driver.findElement(By.xpath("//input[@name = 'password']")).sendKeys("qazwsx");;
-//		driver.findElement(By.xpath("//button/span[text()= 'Đăng nhập']")).click();
-
-//	}
+		 driver.findElement(By.cssSelector(".cursor-pointer")).click();
+		 driver.findElement(By.xpath("//a[text() = 'Đăng xuất']")).click();
+	}
+	@Test
+	public void TC_02_Search() throws InterruptedException {
+		driver.findElement(By.xpath("//input[@name = 'email']")).sendKeys("bkaadmin");;
+		driver.findElement(By.xpath("//input[@name = 'password']")).sendKeys("qazwsx");;
+		driver.findElement(By.xpath("//button/span[text()= 'Đăng nhập']")).click();
+		driver.findElement(By.xpath("//span[text()='Thư viện số']")).click();
+		driver.findElement(By.xpath("//span[text()='Danh sách nội dung số']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//button[text() = ' Filter ']")).click();
+		driver.findElement(By.xpath("//label[text()= 'Bán nội dung']/parent::div//input[@placeholder = 'Select']")).click();
+		List<WebElement> typeList = new ArrayList<>();
+		typeList = driver.findElements(By.cssSelector("#el-id-8321-806 li span"));
+		
+		for(WebElement tmp : typeList) {
+			if(tmp.getText().equals("Có bán nội dung")) {
+				tmp.click();
+				System.out.print("hi");
+			}
+		}
+		driver.findElement(By.xpath("//span[text() = ' Lưu ']")).click();
+		Assert.assertFalse(driver.findElement(By.xpath("//h2[text()= 'Filter danh sách']")).isDisplayed());
+	}
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
